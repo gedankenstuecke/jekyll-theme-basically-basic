1428e5cd917b33ce617a7ea64a78aeea168b0b04
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2023-02-08 15:56:42 +0000

    minimal working test?

---
+++ docs/assets/js/ohana.js
@@ -0,0 +1,13 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+console.log(urlParams);
+
+const lost = urlParams.get('lost')
+
+if (lost){
+    console.log('lost is present');
+    fetch("https://maker.ifttt.com/trigger/ohana_lost/with/key/p4uvQXthhoI-mFbWVi2W6SlzHS-5Pro3wVVvnafnw_a", {mode: 'no-cors'});
+} else {
+    console.log('lost is absent!');
+}
+
---
+++ docs/lost.md
@@ -0,0 +1,15 @@
+---
+title: Oh no, I'm lost!
+layout: page
+image: assets/images/intro-pic.jpg
+permalink: lost/
+introduction: |
+    You probably ended up here after scanning the QR code on my dog tag! So what should you do now?!
+---
+
+# Get in touch with my parents
+
+## Eugenia
+
+
+## Bastian
